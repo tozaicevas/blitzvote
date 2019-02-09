1d19d060fe5b52e146aadc8f4e363c8c7874fbbf
Tomas Zaicevas <tozaicevas@gmail.com>
2019-02-09 18:44:58 +0200

    leftbar

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import { List } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
 
 const LeftBar = () => (
   <List selection verticalAlign="middle">
-    <List.Item icon="users">
+    <List.Item>
+      <Icon name="book" size="large" />
       <List.Content>
-        <List.Header>Helen</List.Header>
+        <List.Header>Svietimas</List.Header>
       </List.Content>
     </List.Item>
-    <List.Item icon="marker" content="New York, NY" />
-    <List.Item
-      icon="mail"
-      content={<a href="mailto:jack@semantic-ui.com">jack@semantic-ui.com</a>}
-    />
-    <List.Item
-      icon="linkify"
-      content={<a href="http://www.semantic-ui.com">semantic-ui.com</a>}
-    />
+    <List.Item>
+      <Icon name="recycle" size="large" />
+      <List.Content>
+        <List.Header>Ekologija</List.Header>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <Icon name="shield" size="large" />
+      <List.Content>
+        <List.Header>Saugumas</List.Header>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <Icon name="computer" size="large" style={{ paddingRight: "0.15em" }} />
+      <List.Content>
+        <List.Header>Technologijos</List.Header>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <Icon name="money" size="large" style={{ paddingRight: "0.15em" }} />
+      <List.Content>
+        <List.Header>Ekonomika</List.Header>
+      </List.Content>
+    </List.Item>
   </List>
 );
 

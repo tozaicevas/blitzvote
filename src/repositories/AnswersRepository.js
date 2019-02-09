48d30b01a970887756f0e887bd1075ae79e900df
Faustas Butkus <faustas.butkus@gmail.com>
2019-02-09 14:41:04 +0200

    Answers repo

---
+++ src/repositories/AnswersRepository.js
@@ -0,0 +1,19 @@
+import Answer from '../models/Answer';
+
+export class AnswerRepository {
+    static data  = [
+        new Answer(1, 1, 'taaaaip'),
+        new Answer(2, 1, 'manau ne, bet gal ir nieko'),
+        new Answer(3, 2, 'hahha, prajuokinai'),
+        new Answer(4, 2, 'krypu krypu'),
+        new Answer(5, 1, 'geras vaizdelis')
+    ];
+
+    getAll = () => {
+        return this.data;
+    }
+
+    insert = (user) => {
+        this.data.push(user);
+    }
+}
--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -2,11 +2,11 @@ import User from '../models/User';
 
 export class UserRepository {
     static data  = [
-        new User(1, true, "Viktor Uspaschik", null, 1),
-        new User(2, true, "Eugenijus Gentvilas", null, 2),
-        new User(3, false, "Faustas Butkus", null, null),
-        new User(4, false, "Tomas Zaicevas", null, null),
-        new User(5, false, "Joris Medeisis", null, null)
+        new User(1, true, 'Viktor Uspaschik', null, 1),
+        new User(2, true, 'Eugenijus Gentvilas', null, 2),
+        new User(3, false, 'Faustas Butkus', null, null),
+        new User(4, false, 'Tomas Zaicevas', null, null),
+        new User(5, false, 'Joris Medeisis', null, null)
     ];
 
     getAll = () => {

0c8c1427b5b8d13beb72bb5b0b716ff108609d70
Unknown <joris.medeisis@gmail.com>
2019-02-09 17:43:13 +0200

    users

---
+++ src/state/reducers/user.js
@@ -0,0 +1,22 @@
+import Answer from "../models/Answer";
+import User from "../../models/User";
+
+const initialState = [
+    new User(1, true, "Viktor Uspaschik", null, 1),
+    new User(2, true, "Eugenijus Gentvilas", null, 2),
+    new User(3, false, "Faustas Butkus", null, null),
+    new User(4, false, "Tomas Zaicevas", null, null),
+    new User(5, false, "Joris Medeisis", null, null)
+];
+
+export default function (action, state = initialState) {
+    switch(action.type) {
+        case 'USER_INSERT':
+            return [
+                ...state,
+                action.payload
+            ];
+        default:
+            return state;
+    }
+}

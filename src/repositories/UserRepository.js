6e2db55f6c2fe8e3ee9a66474f6d9024d76b114f
Faustas Butkus <faustas.butkus@gmail.com>
2019-02-09 14:31:50 +0200

    user repo

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,17 @@
+export class UserRepository {
+    static data  = [
+        new User(1, true, "Viktor Uspaschik", null, 1),
+        new User(2, true, "Eugenijus Gentvilas", null, 2),
+        new User(3, false, "Faustas Butkus", null, null),
+        new User(4, false, "Tomas Zaicevas", null, null),
+        new User(5, false, "Joris Medeisis", null, null)
+    ];
+
+    getAll() {
+        return this.data;
+    }
+
+    insert(user) {
+        this.data.push(user);
+    }
+}

a5fa2ef0a71568c520d9a108221d9909d7b53172
Tomas Zaicevas <tozaicevas@gmail.com>
2019-02-09 14:20:23 +0200

    partyId in user

--- src/models/User.jsx
+++ src/models/User.jsx
@@ -1,8 +1,15 @@
+import Party from "";
+
 export default class User {
-  constructor(id, isPolitician, name, photo) {
+  constructor(id, isPolitician, name, photo, partyId) {
     this.id = id;
     this.isPolitician = isPolitician;
     this.name = name;
     this.photo = photo;
+    this.partyId = partyId;
+  }
+
+  getParty() {
+    Party.getAllParties();
   }
 }

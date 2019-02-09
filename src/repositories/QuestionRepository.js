6312a9c9ac5e15aca8a7a39da2c7116ebc35317a
Faustas Butkus <faustas.butkus@gmail.com>
2019-02-09 16:25:05 +0200

    dummy  data changed

--- src/repositories/QuestionRepository.js
+++ src/repositories/QuestionRepository.js
@@ -3,17 +3,10 @@ import Question from "../models/Question";
 export default class QuestionRepository {
   static data = [
     new Question(1, 4, "Ką manote apie bendrą ES kariuomenę?", 2, 5, null),
-    new Question(1, 4, "Ar jūs už dyzelinių automobilių ribojimą?", 1, 1, null),
-    new Question(
-      1,
-      4,
-      "Ar švietimas turi būti suvienodintas tarp valstybių?",
-      3,
-      2,
-      null
-    ),
-    new Question(1, 4, "Ar Vengrija nusipelnė ES sankcijų?", 5, 4, null),
-    new Question(1, 4, "Kodėl ES negali bendradarbiauti su Rusija?", 1, 3, null)
+    new Question(2, 4, "Ar jūs už dyzelinių automobilių ribojimą?", 1, 1, null),
+    new Question(3, 4, "Ar švietimas turi būti suvienodintas tarp valstybių?", 3, 2, null),
+    new Question(4, 4, "Ar Vengrija nusipelnė ES sankcijų?", 5, 4, null),
+    new Question(5, 4, "Kodėl ES negali bendradarbiauti su Rusija?", 1, 3, null)
   ];
 
   static getAll = () => {

56d298585ecda3ed5a09b3669024b06076aa97ff
Faustas Butkus <faustas.butkus@gmail.com>
2019-02-09 17:34:52 +0200

    answers reducers

---
+++ src/state/reducers/answers.js
@@ -0,0 +1,21 @@
+import Answer from "../models/Answer";
+
+const initialState = [
+    new Answer(1, 1, "taaaaip"),
+    new Answer(2, 1, "manau ne, bet gal ir nieko"),
+    new Answer(3, 2, "hahha, prajuokinai"),
+    new Answer(4, 2, "krypu krypu"),
+    new Answer(5, 1, "geras vaizdelis")
+];
+
+export default function (action, state = initialState) {
+    switch(action.type) {
+        case 'ANSWER_INSERT': 
+            return [
+                ...state,
+                action.payload
+            ];
+        default:
+            return state;
+    }
+}

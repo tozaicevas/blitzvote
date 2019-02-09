3dd1febe72ccf12eb166e85a8cd90ac7705367fa
Unknown <joris.medeisis@gmail.com>
2019-02-09 15:56:22 +0200

    candidates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Router, Switch, Route } from "react-router";
 import { history } from "./state";
 import Layout from "./components/Layout";
 import "./App.css";
+import Candidates from "./views/Candidates";
 import MainView from "./views/Main";
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={MainView} />
             </Switch>
+                <Route exact path="/" component={Candidates}/>
           </Layout>
         </Router>
       </div>

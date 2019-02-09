f06f961308457e235f36a3a10f2e0011d0aad062
Faustas Butkus <faustas.butkus@gmail.com>
2019-02-09 13:03:55 +0200

    Layout stuff

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,5 @@
 import React from 'react'
-import {
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Image,
-  List,
-  Menu,
-  Segment,
-} from 'semantic-ui-react'
+import { Container, Header, Image, Menu } from 'semantic-ui-react'
 
 const Layout = () => (
   <div style={{ height: '100%' }}>
@@ -23,31 +14,8 @@ const Layout = () => (
     </Menu>
 
     <Container text style={{ marginTop: '7em' }}>
-      <Header as='h1'>Semantic UI React Fixed Template</Header>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>
-        A text container is used for the main container, which is useful for single column layouts.
-      </p>
+      
     </Container>
-
-    <Segment inverted vertical style={{ margin: '5em 0em 0em' }}>
-      <Container textAlign='center'>
-        <List horizontal inverted divided link size='small'>
-          <List.Item as='a' href='#'>
-            Site Map
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Contact Us
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Terms and Conditions
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Privacy Policy
-          </List.Item>
-        </List>
-      </Container>
-    </Segment>
   </div>
 )
 

e9ad2cdad8928482d7027afb84d66d07b7f4d834
Unknown <joris.medeisis@gmail.com>
2019-02-10 13:28:21 +0200

    dwsddf

--- src/state/reducers/answers.js
+++ src/state/reducers/answers.js
@@ -4,8 +4,8 @@ const initialState = [
     new Answer(1, 11, "Gynyba kaip niekada svarbi, tačiau dabar turime NATO.", 1),
     new Answer(2, 9, "Valstybės turėtų mokytis iš kitų gerųjų pavyzdžių, tačiau išlaikyti kultūrinį identitetą.", 3),
     new Answer(3, 4, "Taip, nes antros žemės neturime.", 2),
-    new Answer(4, 6, "Šiuo metu ne, kol kas ES ekologijos reguliavimas yra vienas griežčiausių pasaulyje.", 2),
-    new Answer(5, 6, "Žinoma, greičiau tapsime nepriklasomi nuo iškastinio kuro.", 2)
+    new Answer(4, 7, "Šiuo metu ne, kol kas ES ekologijos reguliavimas yra vienas griežčiausių pasaulyje.", 2),
+    new Answer(5, 12, "Žinoma, greičiau tapsime nepriklasomi nuo iškastinio kuro.", 2)
 ];
 
 export default function (state = initialState, action) {

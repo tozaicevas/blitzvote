e0f896141ff147ce1779aaa72c59294c8a792750
Tomas Zaicevas <tozaicevas@gmail.com>
2019-02-10 09:53:11 +0200

    candidate hotfix

--- src/components/candidates/CandidateCard.js
+++ src/components/candidates/CandidateCard.js
@@ -1,31 +1,26 @@
-import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react'
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-const CandidateCard = ({candidate}) => {
-    console.log(candidate);
-    return (
-        <Card>
-            <Image src={candidate.photo} size='medium'/>
-            <Card.Content>
-                <Card.Header>{candidate.name}</Card.Header>
-                <Card.Meta>
-                    {candidate.subtitle}
-                </Card.Meta>
-                <Card.Description>{candidate.party}</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <a>
-                    <Icon name='question circle'/>
-                    3 atsakyti klausimai
-                </a>
-            </Card.Content>
-            <Card.Content extra>
-                <a>
-                    <Icon name='phone volume'/>
-                    Paskutinį kart aktyvus prieš 6 valandas
-                </a>
-            </Card.Content>
-        </Card>
-    );
+const CandidateCard = ({ candidate }) => {
+  return (
+    <Card>
+      <Image src={candidate.photo} size="medium" />
+      <Card.Content>
+        <Card.Header>{candidate.name}</Card.Header>
+        <Card.Meta>{candidate.subtitle}</Card.Meta>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="question circle" />3 atsakyti klausimai
+        </a>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="phone volume" />
+          Paskutinį kart aktyvus prieš 6 valandas
+        </a>
+      </Card.Content>
+    </Card>
+  );
 };
-export default CandidateCard
+export default CandidateCard;

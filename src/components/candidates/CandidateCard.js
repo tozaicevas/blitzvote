6856d918894164eae08395014e7afb4d8c43f4a0
Faustas Butkus <faustas.butkus@gmail.com>
2019-02-10 13:34:05 +0200

    aaa

--- src/components/candidates/CandidateCard.js
+++ src/components/candidates/CandidateCard.js
@@ -4,28 +4,30 @@ import { NavLink } from 'react-router-dom';
 
 const CandidateCard = ({ candidate }) => {
   return (
-    <Card>
-      <Image
-        src={candidate.photo}
-        size="medium"
-        style={{ height: "15em", cursor: "pointer" }}
-      />
-      <Card.Content>
-        <Card.Header style={{ cursor: "pointer" }}>
-          {candidate.name}
-        </Card.Header>
-        <Card.Meta>{candidate.subtitle}</Card.Meta>
-      </Card.Content>
-      <Card.Content extra>
-          <Icon name="question circle" />
-          {Math.floor(Math.random() * 3) + 1} atsakyti klausimai
-      </Card.Content>
-      <Card.Content extra>
-          <Icon name="phone volume" />
-          Paskutinį kart aktyvus prieš {Math.floor(Math.random() * 9) + 1}{" "}
-          valandas
-      </Card.Content>
-    </Card>
+    <NavLink to={"/kandidatai/" + candidate.id} style={{ marginLeft: 10 }} >
+      <Card>
+        <Image
+          src={candidate.photo}
+          size="medium"
+          style={{ height: "15em", cursor: "pointer" }}
+        />
+        <Card.Content>
+          <Card.Header style={{ cursor: "pointer" }}>
+            {candidate.name}
+          </Card.Header>
+          <Card.Meta>{candidate.subtitle}</Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
+            <Icon name="question circle" />
+            {Math.floor(Math.random() * 3) + 1} atsakyti klausimai
+        </Card.Content>
+        <Card.Content extra>
+            <Icon name="phone volume" />
+            Paskutinį kart aktyvus prieš {Math.floor(Math.random() * 9) + 1}{" "}
+            valandas
+        </Card.Content>
+      </Card>
+    </NavLink>
   );
 };
 export default CandidateCard;
